74c98f548f476a19c35c86f562ae854d716954cb
Undetaker2005 <vitalik.stayoha2005@gmail.com>
2024-02-27 22:08:20 +0200

    add project files

---
+++ LR_Veb/Dropmenu.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/per.css">
+    <link rel="stylesheet" href="css/less-dropmenu.css">
+    <title>LR_1</title>
+</head>
+<body>
+    <header>
+        <div class = "menu">
+            <div class = "logo">
+                <a href="#"><img src="img/logo.png" alt="logo"></a>
+            </div>
+            <div class = "menu-link">
+                <div class="contact-buttons">
+                    <a href="" class="phone-button">
+                        <i class="fas fa-phone-alt"></i><span></span>Телефон
+                    </a>
+                    <a href="https://web.telegram.org/a/#1968603785" class="telegram-button">
+                        <i class="fab fa-telegram-plane"></i> Телеграм
+                    </a>
+                </div>
+                <div class="dropdown">
+                    <button class="dropbtn">Menu<i class="fas fa-bars" style = "margin: 0 5px;"></i></button>
+                    <div class="dropdown-content">
+                        <a href="#">Location</a>
+                        <a href="#">Price</a>
+                        <a href="#">Discounts</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="block-top">
+            <h3>Golden Time</h3>
+            <h2>The Best <br> Night Travels</h2>
+            <nav>
+                <button class="a-pulse">More</button>
+                <button class="a-pulse">More</button>
+            </nav>
+        </div>
+    </header>
+    <div class="info">
+        <main class = "info-top">
+            <h1>Golden Time</h1>
+            <h2>Golden Time</h2>
+            <p>There are many variations of passages of Lorem Ipsum</p>
+        </main>
+        <div class="main-content-top">
+            <main>
+                <article class = "new-top">
+                    <nav>
+                        <img src="img/img2.png" alt="img">
+                        <div>
+                            <h1>Bonorum et Malorum</h1>
+                            <p>There are many variations of passages of Lorem Ipsum</p>
+                        </div>
+                    </nav>
+                    <nav>
+                        <img src="img/img3.png" alt="img">
+                        <div>
+                            <h1>Bonorum et Malorum</h1>
+                            <p>There are many variations of passages of Lorem Ipsum</p>
+                        </div>
+                    </nav>
+                    <nav>
+                        <img src="img/img4.png" alt="img">
+                       <div>
+                            <h1>Bonorum et Malorum</h1>
+                            <p>There are many variations of passages of Lorem Ipsum</p>
+                        </div>
+                    </nav>
+                </article>
+                <article class = "new-bottom">
+                    <nav>
+                        <img src="img/img2.png" alt="img">
+                        <div>
+                            <h1>Bonorum et Malorum</h1>
+                            <p>There are many variations of passages of Lorem Ipsum</p>
+                        </div>
+                    </nav>
+                    <nav>
+                        <img src="img/img3.png" alt="img">
+                        <div>
+                            <h1>Bonorum et Malorum</h1>
+                            <p>There are many variations of passages of Lorem Ipsum</p>
+                        </div>
+                    </nav>
+                    <nav>
+                        <img src="img/img4.png" alt="img">
+                        <div>
+                            <h1>Bonorum et Malorum</h1>
+                            <p>There are many variations of passages of Lorem Ipsum</p>
+                        </div>
+                    </nav>
+                </article>           
+            </main>
+        </div>
+        <div class="main-content-bottom">
+            <article class="block-info">
+                <nav> 
+                    <div class="text-25">
+                        <h1>Golden Time</h1>
+                        <h2>Lorem ipsum logo</h2>
+                        <p>At vero eos et accusamus et iusto odio dignissimos <br> 
+                        ducimus qui blandittis praesentium volu <br> 
+                        deleniti atque corr.</p>
+                        <button>More</button>
+                    </div> 
+                        <img src="img/img5.png" alt="img" class="img-right">
+                </nav>
+                <nav>
+                        <img src="img/img6.png" alt="img" class="img-left">
+                    <div class="text-25">
+                        <h1 >Golden Time</h1>
+                        <h2>Lorem ipsum logo</h2>
+                        <p>At vero eos et accusamus et iusto odio dignissimos <br> 
+                        ducimus qui blandittis praesentium volu <br> 
+                        deleniti atque corr.</p>
+                        <button>More</button>
+                    </div>
+                </nav>
+            </article>
+        </div>
+        <nav class="footer-img"></nav>
+        <footer class="footer">
+            <div class="footer-content">
+                <div class="logo-container">
+                    <img src="img/logo.png" alt="logo" class="logo"><a>Golden Time</a>
+                </div>
+                    <div class="footer-text">
+                        <p>St. Applied City Country</p>
+                        <p>(123) 456-858-78</p>
+                        <p>© 2023 Golden Time</p>
+                    </div>
+                <div class="links-container">
+                    <ul class="footer-links">
+                        <li><a href="#">Our Projects</a></li>
+                        <li><a href="#">FAQ's</a></li>
+                        <li><a href="#">News and Updates</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
